bda6f008ee8a8e349aa722a014ddb6d8b1fca4fa
EvanOdell <evanodell91@gmail.com>
2017-02-02 13:56:22 +0000

    commit

--- Oyster Spend.R
+++ Oyster Spend.R
@@ -1,7 +1,7 @@
 
 
 library(readr)
-bike_data <- read_csv("Cycling and Oyster.csv", 
+bike_data <- read_csv("Cycling and Oyster.csv",
                                     col_types = cols(Date = col_date(format = "%d/%m/%Y")))
 
 library(zoo)
@@ -14,7 +14,11 @@ summary(bike_data)
 
 "Average Monthly Travelcard Cost per Day" <- 126.80/30
 
+oyster_card <- 126.80/30
+
 "Average Bicycle Cost per Day" <- mean(bike_data[["Bike"]])
+
+bike_average <- mean(bike_data[["Bike"]])
 
 travel_summary <- data.frame(bike_total = sum(bike_data[["Bike"]]),
                          oyster_total = sum(bike_data[["Oyster"]]),
@@ -22,7 +26,7 @@ travel_summary <- data.frame(bike_total = sum(bike_data[["Bike"]]),
                          travelcard_total = nrow(bike_data) * oyster_card
 )
 
-#total_savings <- 
+#total_savings <-
 
 total_savings <- sprintf("%.2f", round(travel_summary$travelcard_total - travel_summary$current_total, 2))
 
@@ -34,7 +38,7 @@ travel_summary$class <- NULL
 
 ## Recoding travel_summary$variable into travel_summary$variable
 ## Reordering travel_summary$variable
- 
+
 travel_summary$variable <- as.character(travel_summary$variable)
 travel_summary$variable[travel_summary$variable == "bike_total"] <- "Bike Total"
 travel_summary$variable[travel_summary$variable == "oyster_total"] <- "Oyster Total"
@@ -56,7 +60,7 @@ oyster_roll_gg$Date <- as.Date(oyster_roll_gg$Date)
 
 oyster_roll_gg$Bike_plus_Oyster <- mean(bike_data[["Bike"]]) + oyster_roll_gg$Oyster_Charge
 
-oyster_roll_gg<- melt(oyster_roll_gg, id="Date") 
+oyster_roll_gg<- melt(oyster_roll_gg, id="Date")
 
 ## Recoding oyster_roll_gg$variable into oyster_roll_gg$variable
 oyster_roll_gg$variable <- as.character(oyster_roll_gg$variable)
@@ -65,59 +69,57 @@ oyster_roll_gg$variable[oyster_roll_gg$variable == "Bike_plus_Oyster"] <- "Oyste
 oyster_roll_gg$variable <- factor(oyster_roll_gg$variable)
 
 
-pound <- function(x) { 
-  paste0("£",format(x, big.mark = " ", 
-                    decimal.mark = ",", 
-                    trim = TRUE, scientific = FALSE)) 
+pound <- function(x) {
+  paste0("£",format(x, big.mark = " ",
+                    decimal.mark = ",",
+                    trim = TRUE, scientific = FALSE))
   }
 
-p1 <- ggplot(oyster_roll_gg, aes(x=Date)) + 
-  geom_hline(aes(yintercept=bike_average, linetype="Average Bicycle Cost per Day"), colour = "#b5000e", size=1) + 
-  geom_hline(aes(yintercept=oyster_card,linetype="Average Monthly Travelcard Cost per Day"), color = "#01e245", size=1) + 
-  scale_linetype_manual(values = c(2, 2), 
-                        guide = guide_legend(title = NULL, override.aes = list(color = c("#b5000e", "#01e245")))) + 
-  geom_line(aes(y=value, col = variable), size=1) +  
-  scale_color_discrete("") + 
+p1 <- ggplot(oyster_roll_gg, aes(x=Date)) +
+  geom_hline(aes(yintercept=bike_average, linetype="Average Bicycle Cost per Day"), colour = "#b5000e", size=1) +
+  geom_hline(aes(yintercept=oyster_card,linetype="Average Monthly Travelcard Cost per Day"), color = "#01e245", size=1) +
+  scale_linetype_manual(values = c(2, 2),
+                        guide = guide_legend(title = NULL, override.aes = list(color = c("#b5000e", "#01e245")))) +
+  geom_line(aes(y=value, col = variable), size=1) +
+  scale_color_discrete("") +
   #geom_text(aes(x=min(Date), y=bike_average, label = "Bicycle Cost",hjust = "left", vjust = -0.5)) +
   #geom_text(aes(x=min(Date), y=oyster_card, label = "Monthly Zone 1-2 Travel Card", hjust = "left", vjust = -0.5)) +
-  scale_x_date(date_breaks = "1 week") + 
-  scale_y_continuous(name="Charge over previous 7 days", labels = pound) + 
-  theme(legend.position = "bottom", axis.text.x = element_text(angle = 30, hjust = 1)) + 
-  ggtitle("Cycling vs Travelcard") + 
+  scale_x_date(date_breaks = "1 week") +
+  scale_y_continuous(name="Charge over previous 7 days", labels = pound) +
+  theme(legend.position = "bottom", axis.text.x = element_text(angle = 30, hjust = 1)) +
+  ggtitle("Cycling vs Travelcard") +
   guides(col = guide_legend(ncol=2,nrow = 2, byrow = TRUE)) +
-  geom_text(aes(label = "Bicycle Cost", x = max(Date), y = bike_average, hjust= "right", vjust = 1)) + 
+  geom_text(aes(label = "Bicycle Cost", x = max(Date), y = bike_average, hjust= "right", vjust = 1)) +
   geom_text(aes(label = "Monthly Zone 1-2 Travelcard", x = max(Date), y = oyster_card, hjust= "right", vjust = 1))
 
 p1
 
-gt <- ggplot_gtable(ggplot_build(p1))
-gt$layout$clip[gt$layout$name == "panel"] <- "off"
-grid.draw(gt)
 
 
-p2 <- ggplot(travel_summary, aes(x=variable, y=value, fill=variable, label = value)) + 
-  geom_bar(stat = "identity", position = position_dodge(width=0.5)) + 
-  geom_text(aes(y = value + 0.1, label=paste0("test", value)), position = position_dodge(0.9), vjust = -0.25) + 
-  scale_y_continuous(name="Total Spending from 2016-06-30 to 2017-01-31", labels = pound) + 
-  scale_x_discrete(name="Type of Spending") + 
+
+p2 <- ggplot(travel_summary, aes(x=variable, y=value, fill=variable, label = value)) +
+  geom_bar(stat = "identity", position = position_dodge(width=0.5)) +
+  geom_text(aes(y = value + 0.1, label=paste0("#", value)), position = position_dodge(0.9), vjust = -0.25) +
+  scale_y_continuous(name="Total Spending from 2016-06-30 to 2017-01-31", labels = pound) +
+  scale_x_discrete(name="Type of Spending") +
   ggtitle("Total Cost of Different Transport Modes") +
-  theme(legend.position = "bottom") + 
-  annotate("text", x = 1.5, y = 750, label=paste0("Total Savings: ?", total_savings)) + 
+  theme(legend.position = "bottom") +
+  annotate("text", x = 1.5, y = 750, label=paste0("Total Savings: £", total_savings)) +
   annotate("rect", xmin = 1, xmax = 2.0, ymin = 720, ymax = 780,
-               alpha = .2, colour = "blue", fill = "blue") + 
-  scale_fill_discrete("") 
+               alpha = .2, colour = "blue", fill = "blue") +
+  scale_fill_discrete("")
 
 p2
 
 
 multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
   library(grid)
-  
+
   # Make a list from the ... arguments and plotlist
   plots <- c(list(...), plotlist)
-  
+
   numPlots = length(plots)
-  
+
   # If layout is NULL, then use 'cols' to determine layout
   if (is.null(layout)) {
     # Make the panel
@@ -126,20 +128,20 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
     layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
                      ncol = cols, nrow = ceiling(numPlots/cols))
   }
-  
+
   if (numPlots==1) {
     print(plots[[1]])
-    
+
   } else {
     # Set up the page
     grid.newpage()
     pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-    
+
     # Make each plot, in the correct location
     for (i in 1:numPlots) {
       # Get the i,j matrix positions of the regions that contain this subplot
       matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-      
+
       print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
                                       layout.pos.col = matchidx$col))
     }
