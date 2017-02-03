d067a758c39690a010bedb4448c847b0f74a5339
EvanOdell <evanodell91@gmail.com>
2017-02-03 12:26:00 +0000

    new charts

--- Oyster Spend.R
+++ Oyster Spend.R
@@ -103,7 +103,7 @@ p2 <- ggplot(travel_summary, aes(x=variable, y=value, fill=variable, label = val
   ggtitle("Total Cost of Different Transport Modes") +
   theme(legend.position = "bottom") +
   annotate("text", x = 1.5, y = 750, label=paste0("Difference: £", total_savings), fontface = "bold") +
-  annotate("rect", xmin = 0.9, xmax = 2.1, ymin = 720, ymax = 780,
+  annotate("rect", xmin = 1.1, xmax = 1.9, ymin = 720, ymax = 780,
                alpha = .2, colour = "blue", fill = "blue") +
   scale_fill_discrete("")
 
