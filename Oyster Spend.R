d527ab19f90189c6655ab07d6775971b4b88180e
EvanOdell <evanodell91@gmail.com>
2017-02-22 16:44:35 +0000

    up to date, new format for dates in .csv

--- Oyster Spend.R
+++ Oyster Spend.R
@@ -4,6 +4,11 @@ library(readr)
 bike_data <- read_csv("Cycling and Oyster.csv",
                                     col_types = cols(Date = col_date(format = "%d/%m/%Y")))
 
+## If other format
+bike_data <- read_csv("Cycling and Oyster.csv",
+                      col_types = cols(Date = col_date(format = "%Y-%m-%d")))
+
+
 library(zoo)
 library(ggplot2)
 library(data.table)
@@ -106,44 +111,3 @@ p2 <- ggplot(travel_summary, aes(x=variable, y=value, fill=variable, label = val
 p2
 
 
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  library(grid)
-
-  # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
-
-  numPlots = length(plots)
-
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                     ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-
-  if (numPlots==1) {
-    print(plots[[1]])
-
-  } else {
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-
-    # Make each plot, in the correct location
-    for (i in 1:numPlots) {
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                      layout.pos.col = matchidx$col))
-    }
-  }
-}
-
-multiplot(p1,p2,cols=1)
-
-require(ggplot2)
-require(gridExtra)
-library(ggthemes)
